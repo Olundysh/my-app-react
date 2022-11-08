cf5f44987ab4d28baeeb103c06023c3d2b6cca48
olundysh <olgavecholga@gmail.com>
2022-11-08 10:52:00 +0300

    поменяла текст в слайдере

--- src/components/textSection/TextSection.jsx
+++ src/components/textSection/TextSection.jsx
@@ -21,22 +21,22 @@ const TextSection = () => {
       <h2 className={style.h2_slider}>About</h2>
       <Slider {...settings}>
         <div>
-          <h3 className={style.slider}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rem inventore dolor obcaecati, blanditiis tempora accusantium doloremque possimus perferendis earum repudiandae natus animi sapiente eos qui magni quo quos sunt.</h3>
+          <h3 className={style.slider}>The Tocharian (sometimes Tokharian) languages, also known as Arśi-Kuči, Agnean-Kuchean or Kuchean-Agnean, are an extinct branch of the Indo-European language family spoken by inhabitants of the Tarim Basin, the Tocharians.</h3>
         </div>
         <div>
-          <h3 className={style.slider}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, ab modi. Natus assumenda fuga inventore illo ipsam, quasi similique provident iusto. Laboriosam enim doloremque culpa perferendis alias quas cupiditate quod?</h3>
+          <h3 className={style.slider}>The languages are known from manuscripts dating from the 5th to the 8th century AD, which were found in oasis cities on the northern edge of the Tarim Basin (now part of Xinjiang in northwest China) and the Lop Desert.</h3>
         </div>
         <div>
-          <h3 className={style.slider}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse totam architecto sint! Aliquid rem voluptatibus asperiores commodi unde, culpa similique vitae? Atque voluptate ipsam natus modi culpa reprehenderit, impedit tempore!</h3>
+          <h3 className={style.slider}>The discovery of these languages in the early 20th century contradicted the formerly prevalent idea of an east–west division of the Indo-European language family as Centum and satem languages.</h3>
         </div>
         <div>
-          <h3 className={style.slider}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolorum dignissimos tempora harum in vel maxime deserunt! In cum perspiciatis, voluptates nam rem voluptatem, officia consequatur necessitatibus officiis qui minus.</h3>
+          <h3 className={style.slider}>Scholars studying these manuscripts in the early 20th century identified their authors with the Tokharoi, a name used in ancient sources for people of Bactria (Tokharistan). Although this identification is now believed to be mistaken, "Tocharian" remains the usual term for these languages.</h3>
         </div>
         <div>
-          <h3 className={style.slider}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error minus sed hic vero, ratione cupiditate harum in, tempore fugiat explicabo doloribus cumque. Hic eveniet libero nam rerum modi sapiente pariatur?</h3>
+          <h3 className={style.slider}>The oldest extant manuscripts in Tocharian B are now dated to the 5th or even late 4th century AD, making Tocharian a language of Late Antiquity contemporary with Gothic, Classical Armenian, and Primitive Irish.</h3>
         </div>
         <div>
-          <h3 className={style.slider}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo optio consectetur nesciunt voluptate nemo eligendi quaerat delectus quibusdam error, numquam esse vero. Magni, perferendis autem. Aliquid hic recusandae quos animi?</h3>
+          <h3 className={style.slider}>Tocharian is documented in manuscript fragments, mostly from the 8th century (with a few earlier ones) that were written on palm leaves, wooden tablets, and Chinese paper, preserved by the extremely dry climate of the Tarim Basin.</h3>
         </div>
       </Slider>
     </div>
